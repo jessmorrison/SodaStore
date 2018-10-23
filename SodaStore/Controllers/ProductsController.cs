a5c54c6da0dd1caf36bf83433b9693860ce8f02a
JM <jessmorrison90@gmail.com>
2018-10-23 11:05:05 -0500

    Initial commit

---
+++ SodaStore/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SodaStore.Models;
+namespace SodaStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index(int? id)
+        {
+            if (id == 1)
+            {
+                ProductViewModel model = new ProductViewModel
+                {
+                    Name = "Exotic Soda",
+                    Description = "This soda is delicious",
+                    Price = 2.99m,
+                    ImagePath = "~/images/exotic_soda.jpg"
+                };
+                return View(model);
+            }
+            else if (id == 2)
+            {
+                ProductViewModel model = new ProductViewModel
+                {
+                    Name = "Green Cream Soda",
+                    Description = "This is the greenest soda on the market",
+                    Price = 3.99m,
+                    ImagePath = "~/images/green_cream.jpg"
+                };
+                return View(model);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ SodaStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SodaStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
